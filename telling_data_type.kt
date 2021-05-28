1721c6f94f7fa25119faaddc89ba5ddc1b520c37
Bhushan-shrirame <44223841+Bhushan-shrirame@users.noreply.github.com>
2021-05-28 14:03:31 +0530

    Create telling_data_type.kt

---
+++ telling_data_type.kt
@@ -0,0 +1,13 @@
+
+// To tell the data type to the programe 
+fun main (){
+    var lan:String
+    lan = "Bhushan"
+    println("your name is "+lan)
+    // OR
+    println("your name is $lan') 
+}
+
+/*Output  
+  your name is Bhushan
+  your name is Bhushan*/
