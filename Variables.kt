71f3a4bae6d5af43805863614a0bfb412239aa9b
Bhushan-shrirame <44223841+Bhushan-shrirame@users.noreply.github.com>
2021-05-28 14:01:41 +0530

    Create Variables.kt

---
+++ Variables.kt
@@ -0,0 +1,13 @@
+// Kotlin variable //
+
+/* Variable refers to a memory location. It is used to store data. The  data of variable can
+be change and reused depending on condition or on information passed to the programe */
+
+val name = "bhushan" 
+// here the "val" is "value" it will be fixed when  assign the values
+
+var marks = 28
+marks = 30 
+// here the var is "variable" it can be change over the time when needed
+
+// IMPORTANT when assign the value to the string you should always put the value in double inverted comma.
